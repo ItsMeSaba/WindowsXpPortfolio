dc7dee3c5bf8a8c64c09babf0673d3e5efaa9058
Saba Silagadze <sbsilagadze@gmail.com>
2025-06-26 20:09:26 +0400

    feat: updates cv

--- src/components/Destkop.tsx
+++ src/components/Destkop.tsx
@@ -8,13 +8,7 @@ import folder from "../assets/images/icons/folder.ico";
 import file from "../assets/images/icons/file.ico";
 import adobeFile from "../assets/images/icons/adobeFile.png";
 
-// import file from "../../public/sabaSilagadze.pdf"
-
-interface Args {
-  // openProjectsWindow: () => void,
-}
-
-export default function Desktop(args: Args) {
+export default function Desktop() {
   const {
     setProjectsWindow,
     projectsWindow,
@@ -66,16 +60,16 @@ export default function Desktop(args: Args) {
             openProjectsWindow(workExperienceWindow, setWorkExperienceWindow)
           }
         />
-        {/* <DesktopItem image={adobeFile} text="CV" onDoubleClick={() => fetch("../../public/sabaSilagadze.pdf")} /> */}
+
         <a
           className={styles.desktopItem}
-          href="../../public/sabaSilagadze.pdf"
+          href="/sabaSilagadzeCV.pdf"
           target="_blank"
           download
         >
           <img src={adobeFile} />
 
-          <p>Cv</p>
+          <p>CV</p>
         </a>
       </div>
 
